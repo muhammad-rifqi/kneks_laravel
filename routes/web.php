270ad00c57af4092a5d5b5a1cdeb41ef125e8cda
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-06-13 19:33:38 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,34 @@ Route::get('/', function() {
     return Redirect::to('/login');
 });
 
-Auth::routes();
+// Auth::routes();
+
+// Authentication Routes...
+Route::get('login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+
+// Registration Routes...
+//Route::get('register', [App\Http\Controllers\Auth\RegisterController::class,'showRegistrationForm'])->name('register');
+//Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'register']);
+Route::get('register', function(){
+    return Redirect::to('/login');
+});
+
+// Password Reset Routes...
+Route::get('password/reset', [App\Http\Controllers\Auth\ForgotPasswordController::class,'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [App\Http\Controllers\Auth\ForgotPasswordController::class,'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [App\Http\Controllers\Auth\ResetPasswordController::class,'showResetForm'])->name('password.reset');
+Route::post('password/reset', [App\Http\Controllers\Auth\ResetPasswordController::class, 'reset'])->name('password.update');
+
+// Confirm Password (added in v6.2)
+Route::get('password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class,'showConfirmForm'])->name('password.confirm');
+Route::post('password/confirm', [App\Http\Controllers\Auth\ConfirmPasswordController::class,'confirm']);
+
+// Email Verification Routes...
+Route::get('email/verify', [App\Http\Controllers\Auth\VerificationController::class,'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [App\Http\Controllers\Auth\VerificationController::class,'verify'])->name('verification.verify'); // v6.x
+/* Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify'); // v5.x */
+Route::get('email/resend', [App\Http\Controllers\Auth\VerificationController::class,'resend'])->name('verification.resend');
 
 Route::get('/admin/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
